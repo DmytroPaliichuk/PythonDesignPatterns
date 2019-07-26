59a7ac74f185c79716b3f6a3e379b3d7eb599b99
DmytroPaliichuk <dmitry.paliychuk@gmail.com>
2019-07-26 19:22:17 +0300

    modified Singleton

--- Singleton.py
+++ Singleton.py
@@ -10,21 +10,29 @@ class URLFetcherWithout:
         urls = self.urls
         urls.append(url)
         self.urls = urls
+
+    def show_urls(self):
+        print(self.urls)
     
     
 f1 = URLFetcherWithout()
 f1.fetch('i.ua')
+f1.show_urls()
 
 f2 = URLFetcherWithout()
-f2.fetch('i.ua')
+f2.fetch('google.com')
+f2.show_urls()
 
 print("Is these classes the same: ", f1 is f2)
 
-
+print('=='*30)
+print(' '*12,' SingletoneType ')
+print('=='*30)
 
 
 class SingletoneType(type):
     _instance = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instance:
             cls._instance[cls] = super(SingletoneType, cls).__call__(*args, **kwargs)
@@ -43,3 +51,16 @@ class URLFetcherWith(metaclass=SingletoneType):
         urls.append(url)
         self.urls = urls
 
+    def show_urls(self):
+        print(self.urls)
+
+
+fw1 = URLFetcherWith()
+fw1.fetch('i.ua')
+fw1.show_urls()
+
+fw2 = URLFetcherWith()
+fw2.fetch('google.com')
+fw2.show_urls()
+
+print("Is these classes the same: ", fw1 is fw2)
